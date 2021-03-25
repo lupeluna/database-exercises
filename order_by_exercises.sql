5deca84374976514920a0edb363ecd65588373f9
Lupe Luna <luna.guadalupe898@gmail.com>
2021-03-25 17:21:14 -0500

    order exercises - reupload

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,4 +1,3 @@
-
 use employees;
 
 
@@ -79,4 +78,7 @@ from employees
 	where hire_date like '199%' and birth_date like '%12-25'
 	order by birth_date, hire_date desc;
 	
--- 362 rows Khun Mernini was first and Douadi Pettis was last 
+-- 362 rows Khun Mernini was first and Douadi Pettis was last
+
+
+
